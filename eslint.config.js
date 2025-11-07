37ba2d8feeaed70d092ba89419ca002f50e52a49
talabidele <talabibamidele7@gmail.com>
2025-11-07 15:44:10 +0100

    Added lint file

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+// eslint.config.js
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+export default [
+	js.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		files: ['src/**/*.ts'],
+		languageOptions: {
+			parserOptions: {
+				project: './tsconfig.json',
+			},
+		},
+		rules: {
+			'no-unused-vars': 'warn',
+			'no-console': 'off',
+		},
+	},
+]
